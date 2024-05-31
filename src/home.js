29a6e9eae8c12c510c0ed8f8bc164c7fb59cd04f
lililok <eshanu.sofia@yandex.ru>
2024-05-31 15:28:02 +0300

    home dom+css

--- src/home.js
+++ src/home.js
@@ -1,15 +1,30 @@
+import homeImg from './images/home-img.png';
+
 export function homeLoad() {
     const contentDiv = document.getElementById('content');
 
+    const homeContent = document.createElement('div');
+    homeContent.className = 'home-content';
+
+    const text = document.createElement('div');
+    text.className = 'home-content-text';
+
     const heading = document.createElement('h4');
     heading.textContent = 'Welcome to Sweetie!';
+    heading.className = 'home-heading';
     
     const paragraph = document.createElement('p');
     paragraph.innerHTML = 'A <em>NEW</em> DESSERT EXPERIENCE';
+    paragraph.className = 'home-p';
 
     const img = document.createElement('img');
+    img.src = homeImg;
+    img.alt = 'home img';
+    img.className = 'home-img';
     
-    contentDiv.appendChild(heading);
-    contentDiv.appendChild(paragraph);
-    contentDiv.appendChild(img);
+    homeContent.appendChild(img);
+    text.appendChild(heading);
+    text.appendChild(paragraph);
+    homeContent.appendChild(text);
+    contentDiv.appendChild(homeContent);
 }
